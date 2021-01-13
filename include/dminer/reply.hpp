f308e5d99d649435ca649d8eb5406ed2e419a0f4
Maxime Mouchet <max@maxmouchet.com>
2021-01-13 15:46:24 +0100

    update

--- include/dminer/reply.hpp
+++ include/dminer/reply.hpp
@@ -24,23 +24,24 @@ struct Reply {
   /// @name Probe attributes (IP → ICMP → IP)
   /// @{
   uint32_t inner_dst_ip;  ///< The IP that was targeted by the probe,
-  ///< if we received a reply from this IP,
-  ///< then \ref src_ip == \ref inner_dst_ip.
-  uint16_t inner_size;  ///< The size in bytes of the probe packet.
-  uint8_t inner_ttl;    ///< The TTL of the probe packet.
-  uint8_t inner_proto;  ///< The protocol of the probe packet.
+                          ///< if we received a reply from this IP,
+                          ///< then \ref src_ip == \ref inner_dst_ip.
+  uint16_t inner_size;    ///< The size in bytes of the probe packet.
+  uint8_t inner_ttl;      ///< The TTL of the probe packet.
+  uint8_t inner_proto;    ///< The protocol of the probe packet.
   /// @}
 
   /// @name Probe attributes (IP → ICMP → IP → ICMP/TCP/UDP)
   /// @{
   uint16_t inner_src_port;  ///< The source port of the probe packet.
-  ///< For ICMP probes, we encode the source port
-  ///< in the ICMP checksum and ID fields
-  ///< in order to vary the flow ID.
+                            ///< For ICMP probes, we encode the source port
+                            ///< in the ICMP checksum and ID fields
+                            ///< in order to vary the flow ID.
   uint16_t inner_dst_port;  ///< The destination port of the probe packet,
-  ///< 0 for ICMP probes.
-  uint8_t inner_ttl_from_udp_len;  ///< The TTL that was encoded in the UDP
-  ///< probe packet length, 0 if not an UDP probe.
+                            ///< 0 for ICMP probes.
+  uint8_t
+      inner_ttl_from_udp_len;  ///< The TTL that was encoded in the UDP
+                               ///< probe packet length, 0 if not an UDP probe.
   /// @}
 
   /// @name Estimated attributes
