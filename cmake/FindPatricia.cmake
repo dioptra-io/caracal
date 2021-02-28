ccbe26d04e4a6f09f7d073ec753b1f4a0303b870
Maxime Mouchet <max@maxmouchet.com>
2021-02-28 15:39:56 +0100

    cmake-format: --dangle-parens

--- cmake/FindPatricia.cmake
+++ cmake/FindPatricia.cmake
@@ -1,9 +1,12 @@
 if(NOT TARGET Patricia::Patricia)
   find_package(ZLIB REQUIRED)
   add_library(Patricia::Patricia INTERFACE IMPORTED)
-  target_sources(Patricia::Patricia
-                 INTERFACE "${PROJECT_SOURCE_DIR}/extern/patricia/patricia.cpp")
-  target_include_directories(Patricia::Patricia
-                             INTERFACE "${PROJECT_SOURCE_DIR}/extern/patricia/")
+  target_sources(
+    Patricia::Patricia
+    INTERFACE "${PROJECT_SOURCE_DIR}/extern/patricia/patricia.cpp"
+  )
+  target_include_directories(
+    Patricia::Patricia INTERFACE "${PROJECT_SOURCE_DIR}/extern/patricia/"
+  )
   target_link_libraries(Patricia::Patricia INTERFACE ZLIB::ZLIB)
 endif()
