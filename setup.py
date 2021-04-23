d934f89907f8ed16622f99440a0c901fcc2cc7c6
Maxime Mouchet <max@maxmouchet.com>
2021-04-24 00:23:49 +0200

    update pypy workflow

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@ import sys
 from pathlib import Path
 from skbuild import setup
 
-# Find conan relatively to the current Python installation.
-# This is required (for now) for isolated builds.
+cmake_args = []
+
+# If conan exists in the current Python installation,
+# pass its path to CMake. This solves an issue where
+# the cmake-conan module cannot find conan in some cases.
 conan_cmd = Path(sys.executable).parent / "conan"
+if conan_cmd.exists():
+    cmake_args.append(f"-DCONAN_CMD={conan_cmd}")
+
 
 setup(
     name="pycaracal",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    cmake_args=[f"-DCONAN_CMD={conan_cmd}"],
+    cmake_args=cmake_args,
 )
