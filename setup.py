55c7bf33fdd8763a75b452b0a7a454f24940efd4
Maxime Mouchet <max@maxmouchet.com>
2022-04-15 23:57:23 +0200

    release(project): 0.9.0 → 0.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="pycaracal",
-    version="0.9.0",
+    version="0.9.1",
     author="Maxime Mouchet",
     author_email="max@maxmouchet.com",
     url="https://github.com/dioptra-io/caracal",
