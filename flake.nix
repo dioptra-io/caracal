9defe6ea850c5cf02756b51d6355398b79972316
Maxime Mouchet <max@maxmouchet.com>
2022-04-15 22:11:56 +0200

    release(project): 0.8.1 → 0.9.0

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         packages = {
           caracal = pkgs.stdenv.mkDerivation {
             pname = "caracal";
-            version = "0.8.1";
+            version = "0.9.0";
             src = self;
             nativeBuildInputs = [
               pkgs.cmake
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name="pycaracal",
-    version="0.8.1",
+    version="0.9.0",
     author="Maxime Mouchet",
     author_email="max@maxmouchet.com",
     url="https://github.com/dioptra-io/caracal",
