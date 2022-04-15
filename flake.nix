7844f3e7b3ca43189e8a65810d05d0519b73b6f1
Maxime Mouchet <max@maxmouchet.com>
2022-04-15 20:43:38 +0200

    draft

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "A fast ICMP/UDP IPv4/v6 Paris traceroute and ping engine";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/f06cec9564a2fc6b85d92398551a0c059fc4db86";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlay = final: prev: {
+          boostWithZstd = prev.boost.overrideAttrs (old: {
+            buildInputs = old.buildInputs ++ [prev.zstd];
+          });
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+      in {
+        packages = {
+          caracal = pkgs.stdenv.mkDerivation {
+            pname = "caracal";
+            version = "0.1.0";
+            src = self;
+            nativeBuildInputs = [
+              pkgs.cmake
+            ];
+            buildInputs = [
+              pkgs.boostWithZstd
+              pkgs.cxxopts
+              pkgs.libtins
+              pkgs.libpcap
+              pkgs.spdlog
+            ];
+            cmakeFlags = [
+              "-DWITH_CONAN=OFF"
+              "-DWITH_PYTHON=OFF"
+            ];
+            buildPhase = ''
+              cmake --build . --target caracal-bin
+            '';
+            installPhase = ''
+              cmake --install .
+            '';
+          };
+        };
+        defaultPackage = self.packages.${system}.caracal;
+      }
+    );
+}
