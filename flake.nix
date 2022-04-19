ba0032a4d43363f101fd79aa4962e036d907d315
Maxime Mouchet <max@maxmouchet.com>
2022-04-19 18:26:26 +0200

    release(project): 0.9.1 → 0.9.2

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         packages = {
           caracal = pkgs.stdenv.mkDerivation {
             pname = "caracal";
-            version = "0.9.1";
+            version = "0.9.2";
             src = self;
             nativeBuildInputs = [
               pkgs.cmake
